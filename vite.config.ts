11189b1423fa5c51b4cb231396fa6adda0aaf34a
Ade <adesolasolarin@gmail.com>
2025-11-07 01:34:47 +0100

    thurs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    port: 5173,
-  },
-  build: {
-    outDir: 'dist', // this must match Render's publish directory
-  },
-})
+});
